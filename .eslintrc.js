2b48af3aea7cf6771ec069d80675cfbe4a7e5da7
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-12-07 17:18:56 +0530

    comit code for image refrence

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,8 +22,33 @@ module.exports = {
   },
   plugins: ['react', 'react-native', 'import', 'jsx-a11y', 'react-hooks', 'prettier'],
   rules: {
-    'arrow-parens': ['error', 'as-needed'],
+    'arrow-parens': [1, 'as-needed'],
+    'object-curly-newline': 0,
+    'global-require': 'off',
+
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/state-in-constructor': [1, 'never'],
+    'react/static-property-placement': [
+      'error',
+      'property assignment',
+      {
+        childContextTypes: 'static getter',
+        contextTypes: 'static public field',
+        contextType: 'static public field',
+        displayName: 'static public field',
+        propTypes: 'static public field',
+        defaultProps: 'static public field',
+      },
+    ],
+    'react/jsx-props-no-spreading': 0,
+    'react/button-has-type': 0,
+    'react/jsx-closing-bracket-location': [
+      'error',
+      {
+        nonEmpty: 'after-props',
+        selfClosing: 'tag-aligned',
+      },
+    ],
 
     // React-Hooks Plugin
     'react-hooks/rules-of-hooks': 'error',
--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -1,9 +1,22 @@
+/* eslint-disable global-require */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, ImageBackground } from 'react-native';
+import CakeIcon from '../../assets/icons/cake-black.svg';
 
 const index = () => (
   <View>
     <Text>Splash Screen</Text>
+    <Image
+      style={{ height: 120, width: 120, borderRadius: 10 }}
+      source={require('../../assets/images/64dffaa58ffa55a377cdf42b6a690e721585809275.png')}
+    />
+    <CakeIcon height={24} width={24} fill="red" />
+    <ImageBackground
+      style={{ height: 120, width: 120, borderRadius: 10 }}
+      source={require('../../assets/images/64dffaa58ffa55a377cdf42b6a690e721585809275.png')}>
+      <Text style={{ color: 'red' }}>Hello from image</Text>
+      <CakeIcon height={24} width={24} fill="red" />
+    </ImageBackground>
   </View>
 );
 
