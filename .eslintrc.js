45c919f0473d92340f5119b511dd968c929db00d
Yagnesh Modh <yagneshmodh@Yagneshs-MacBook-Pro.local>
2020-12-07 15:26:15 +0530

    Completed Project Setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,34 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  parser: 'babel-eslint',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier/react',
+    'prettier/standard',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'react-native', 'import', 'jsx-a11y', 'react-hooks', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+
+    // React-Hooks Plugin
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'error',
+
+    // React-Native Plugin
+    'react-native/no-inline-styles': 'warn',
+  },
 };
