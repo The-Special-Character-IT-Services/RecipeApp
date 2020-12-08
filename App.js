0694cb74cc0355bff45dead6f5f9e014fa0e7d65
raj-patel-sys <patelrajj1904@gmail.com>
2020-12-08 16:27:42 +0530

    commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { View, StatusBar } from 'react-native';
 import React from 'react';
 import Splash from './src/screens/Splash';
 import Login from './src/screens/Login';
@@ -7,19 +8,28 @@ import Registration from './src/screens/Registration';
 import Home from './src/screens/Home';
 
 const Stack = createStackNavigator();
-
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'white',
+  },
+};
 const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator
-      screenOptions={{
-        headerShown: false,
-      }}>
-      <Stack.Screen name="Splash" component={Splash} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Registration" component={Registration} />
-      <Stack.Screen name="Home" component={Home} />
-    </Stack.Navigator>
-  </NavigationContainer>
+  <View style={{ flex: 1 }}>
+    <NavigationContainer theme={MyTheme}>
+      <StatusBar backgroundColor="white" barStyle="dark-content" />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Splash" component={Splash} />
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Registration" component={Registration} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  </View>
 );
 
 export default App;
