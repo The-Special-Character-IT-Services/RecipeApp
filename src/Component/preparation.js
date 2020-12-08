01cd6fc31f791a967d535f48b0f4eb9518647188
unknown <patelrajj1904@gmail.com>
2020-12-08 14:54:14 +0530

    Harsh Preparations

---
+++ src/Component/preparation.js
@@ -0,0 +1,103 @@
+/* eslint-disable global-require */
+import React from 'react';
+import { View, Text, ScrollView, ImageBackground, Pressable } from 'react-native';
+// import { ScrollView } from 'react-native-gesture-handler';
+import Timer from '../assets/icons/timer.svg';
+import Play from '../assets/icons/play.svg';
+
+const index = () => {
+  const Loopfood = [
+    {
+      id: 1,
+      text: '1. Melt chocolate',
+      img: require('../assets/image/choco.png'),
+    },
+    {
+      id: 2,
+      text: '2. Squeeze juice',
+      img: require('../assets/image/lemon.png'),
+    },
+    {
+      id: 3,
+      text: '3. South Indian',
+      img: require('../assets/image/dosa.png'),
+    },
+    {
+      id: 4,
+      text: '4. Pizzas',
+      img: require('../assets/image/pizza.png'),
+    },
+    {
+      id: 5,
+      text: '5. Birthday cakes',
+      img: require('../assets/image/cakes.png'),
+    },
+  ];
+
+  return (
+    <View>
+      <View style={{ marginLeft: 36, marginTop: 20 }}>
+        <Text style={{ fontSize: 26 }}>Very Berry Tart</Text>
+        <Text numberOfLines={2} style={{ color: 'gray' }}>
+          Amazing combo of sweet and sour taste! Super easy to cook.Enjoy!
+        </Text>
+      </View>
+      <View
+        style={{
+          marginVertical: 10,
+        }}>
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+          <Text style={{ fontSize: 20, paddingHorizontal: 30, fontWeight: 'bold' }}>
+            Preparation
+          </Text>
+          <View style={{ flexDirection: 'row', padding: 1 }}>
+            <Timer height={24} width={24} fill="black" />
+            <Text style={{ fontSize: 17, fontWeight: '900', paddingRight: 20 }}> 35` </Text>
+          </View>
+        </View>
+        <View style={{ borderRadius: 9, flexDirection: 'column' }}>
+          <ScrollView
+            contentContainerStyle={{ paddingHorizontal: 15 }}
+            horizontal
+            showsHorizontalScrollIndicator={false}>
+            {Loopfood.map(item => (
+              <View
+                style={{ flexDirection: 'row', paddingHorizontal: 5, paddingTop: 10 }}
+                key={item.id}>
+                <ImageBackground
+                  imageStyle={{ borderRadius: 15 }}
+                  source={item.img}
+                  style={{
+                    height: 250,
+                    width: 150,
+                    borderRadius: 500,
+                  }}>
+                  <Text style={{ color: 'white', fontSize: 19, paddingLeft: 10, paddingTop: 5 }}>
+                    {item.text}
+                  </Text>
+                </ImageBackground>
+              </View>
+            ))}
+          </ScrollView>
+          <Pressable
+            onPress={() => alert('hi')}
+            style={{
+              marginHorizontal: 40,
+              marginVertical: 20,
+              height: 40,
+              borderRadius: 20,
+              backgroundColor: '#FD6D3B',
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Play height={24} width={24} fill="white" />
+            <Text style={{ color: 'white' }}>Watch Video</Text>
+          </Pressable>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default index;
