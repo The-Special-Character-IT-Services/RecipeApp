c107c04e90137c17334597679bb873af33be2bfe
shuklakhushi <180770116110@socet.edu.in>
2020-12-09 14:20:25 +0530

    Search page Edited

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,6 +1,7 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable react/no-unused-state */
 import React, { PureComponent } from 'react';
-import { TextInput } from 'react-native';
+import { Text, TextInput, View } from 'react-native';
 
 export default class index extends PureComponent {
   state = {
@@ -15,16 +16,64 @@ export default class index extends PureComponent {
 
   render() {
     return (
-      <TextInput
-        placeholder="Search here"
-        style={{
-          borderColor: 'black',
-          borderWidth: 1,
-          borderRadius: 20,
-          margin: 20,
-        }}
-        onchangeText={this.onchangeText}
-      />
+      <View>
+        <TextInput
+          placeholder="Search here"
+          style={{
+            borderColor: 'black',
+            borderWidth: 1,
+            borderRadius: 20,
+            margin: 20,
+          }}
+          onchangeText={this.onchangeText}
+        />
+        <View style={{ flexDirection: 'row' }}>
+          <Text
+            style={{
+              paddingTop: 5,
+              paddingStart: 10,
+              marginLeft: 24,
+              borderRadius: 8,
+              borderWidth: 2,
+              width: 80,
+              height: 30,
+              alignItems: 'center',
+              borderColor: 'black',
+            }}>
+            Trending
+          </Text>
+          <Text
+            style={{
+              paddingTop: 5,
+              paddingStart: 10,
+              marginLeft: 10,
+              flexDirection: 'row',
+              borderRadius: 8,
+              borderWidth: 2,
+              width: 80,
+              height: 30,
+              alignItems: 'center',
+              borderColor: 'black',
+            }}>
+            Rice Items
+          </Text>
+          <Text
+            style={{
+              paddingTop: 5,
+              paddingStart: 10,
+              marginLeft: 10,
+              flexDirection: 'row',
+              borderRadius: 8,
+              borderWidth: 2,
+              width: 80,
+              height: 30,
+              alignItems: 'center',
+              borderColor: 'black',
+            }}>
+            Sweets
+          </Text>
+        </View>
+      </View>
     );
   }
 }
--- src/screens/TabSearch/index.js
+++ src/screens/TabSearch/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View } from 'react-native';
 import SearchBar from '../../components/Search';
+import Footer from '../../components/Footer';
+import Popular from '../../components/Popular';
 
 const index = () => (
   <View>
     <SearchBar />
+
+    <Popular />
+    <Footer />
   </View>
 );
 
