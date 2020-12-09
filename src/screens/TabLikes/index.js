013d253e73879fd880f53fb09a1ae4ceb7b29943
raj-patel-sys <patelrajj1904@gmail.com>
2020-12-10 00:10:04 +0530

    Tablike almostdone

--- src/screens/TabLikes/index.js
+++ src/screens/TabLikes/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
-import TextEle from '../../components/TextEle';
+import { Dimensions, Image, ScrollView, View } from 'react-native';
+import Ingrediants from '../../components/Ingrediants';
+
+const { height: windowHeight, width: windowWidth } = Dimensions.get('window');
 
 const index = () => (
-  <View>
-    <TextEle>Tab Likes</TextEle>
+  <View style={{ flex: 1, justifyContent: 'flex-end' }}>
+    <ScrollView>
+      <Image
+        style={{
+          height: windowHeight * 0.5,
+          width: windowWidth,
+          position: 'relative',
+          top: 0,
+          left: 0,
+        }}
+        source={require('../../assets/images/MorningCake.jpg')}
+      />
+      <View
+        // eslint-disable-next-line react-native/no-inline-styles
+        style={{
+          borderTopEndRadius: 30,
+          borderTopStartRadius: 30,
+
+          height: windowHeight * 0.6,
+          backgroundColor: '#FFF',
+          zIndex: 1,
+        }}>
+        <Ingrediants>Liked Recipe</Ingrediants>
+      </View>
+    </ScrollView>
   </View>
 );
 
