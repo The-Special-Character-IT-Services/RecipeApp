32da2ee9ce64df3e0dc4a73e4140e3ab83e1f3a7
patelyashdeep <patelyashdeep3110@gmail.com>
2020-12-10 15:43:48 +0530

    changes Video adjust

--- src/screens/RecipeVideo/index.js
+++ src/screens/RecipeVideo/index.js
@@ -1,24 +1,18 @@
 import React from 'react';
-import { View } from 'react-native';
+import { Dimensions } from 'react-native';
 import Video from 'react-native-video';
 import Food from '../../assets/Videos/anotherfood.mp4';
 
+const { height: windowHeight, width: windowWidth } = Dimensions.get('window');
 const index = () => (
-  <View
-    style={{ justifyContent: 'center', alignItems: 'center', flex: 1, flexDirection: 'column' }}>
-    <Video
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        bottom: 0,
-        right: 0,
-        height: 800,
-        width: 800,
-      }}
-      source={Food}
-    />
-  </View>
+  <Video
+    style={{
+      height: windowHeight * 0.5,
+      width: windowWidth,
+    }}
+    resizeMode="cover"
+    source={Food}
+  />
 );
 
 export default index;
