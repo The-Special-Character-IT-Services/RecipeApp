1300df92516da5adbb87d361afa66417f4c5d8e6
raj-patel-sys <patelrajj1904@gmail.com>
2020-12-13 20:03:55 +0530

    changed text color

--- src/components/Preparation/ListItem.js
+++ src/components/Preparation/ListItem.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useTheme } from '@react-navigation/native';
 import { View, Image } from 'react-native';
 import TextEle from '../TextEle';
 
-const ListItem = ({ item, cardWidth }) => (
-  <View key={item.id} style={{ width: cardWidth }}>
-    <View style={{ paddingHorizontal: 5, paddingTop: 10 }}>
-      <Image
-        imageStyle={{ borderRadius: 15 }}
-        source={item.img}
-        style={{
-          height: 230,
-          width: 330,
-          borderRadius: 50,
-        }}
-      />
-      <TextEle variant="caption" style={{ color: 'white', marginTop: 20 }}>
-        {item.text}
-      </TextEle>
+const ListItem = ({ item, cardWidth }) => {
+  const { colors } = useTheme();
+  return (
+    <View key={item.id} style={{ width: cardWidth }}>
+      <View style={{ paddingHorizontal: 5, paddingTop: 10 }}>
+        <Image
+          imageStyle={{ borderRadius: 15 }}
+          source={item.img}
+          style={{
+            height: 230,
+            width: 330,
+            borderRadius: 50,
+          }}
+        />
+        <TextEle variant="caption" style={{ color: colors.text, marginTop: 20 }}>
+          {item.text}
+        </TextEle>
+      </View>
     </View>
-  </View>
-);
+  );
+};
 
 ListItem.propTypes = {
   item: PropTypes.shape({
