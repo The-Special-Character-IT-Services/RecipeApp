87c4667e7aa66df6554321830c49fd6b0f27bcd3
patelyashdeep <patelyashdeep3110@gmail.com>
2020-12-28 16:48:49 +0530

    Header passed as Props

--- src/screens/RecipeVideo/index.js
+++ src/screens/RecipeVideo/index.js
@@ -3,8 +3,8 @@ import { Dimensions, Text, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import Video from 'react-native-video';
 import { Svg } from 'react-native-svg';
+import PropTypes from 'prop-types';
 import { VictoryLabel, VictoryPie } from 'victory-native';
-// import data from './data';
 
 const summary = `Macronutrients: Macronutrients are required daily in large quantities. They include proteins, fats, carbohydrates, some minerals, and water.
 Micronutrients: Micronutrients are required daily in small quantities—in milligrams (one thousandth of a gram) to micrograms (one millionth of a gram). They include vitamins and certain minerals that enable the body to use macronutrients. These minerals are called trace minerals because the body needs only very small amounts.
@@ -22,67 +22,84 @@ const chartData = [
   { nutriText: 'Vitamins', color: 'cyan', weight: 20, unit: 'g', id: 4 },
   { nutriText: 'Fibers', color: 'navy', weight: 20, unit: 'g', id: 5 },
 ];
-const index = () => (
-  <View style={{ flex: 1, backgroundColor: '#ebebeb' }}>
-    <Video
-      paused
-      source={{
-        uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
-      }}
-      style={{
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').width * (9 / 16),
-        backgroundColor: 'black',
-      }}
-      controls
-      resizeMode="cover"
-    />
-    <ScrollView style={{ flex: 1, margin: 15 }}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Nutritional Info.</Text>
-      <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
-        <Svg viewBox="0 0 200 200" height={200} width={200}>
-          <VictoryPie
-            labels={() => null}
-            colorScale={chartData.map(x => x.color)}
-            width={200}
-            height={200}
-            innerRadius={60}
-            data={chartData.map(x => ({ x: x.nutriText, y: x.weight }))}
-          />
-          <VictoryLabel
-            textAnchor="middle"
-            verticalAnchor="middle"
-            x={100}
-            y={100}
-            style={{ fontSize: 14 }}
-            text="71 Cal"
-          />
-        </Svg>
-        <View>
-          {chartData.map(item => (
-            <View style={{ flexDirection: 'row', paddingVertical: 5, alignItems: 'center' }}>
-              <View
-                style={{
-                  height: 10,
-                  width: 30,
-                  backgroundColor: item.color,
-                  borderRadius: 5,
-                  marginRight: 10,
-                }}
-              />
-              <Text>{`${item.weight}${item.unit}`}</Text>
-              <Text style={{ color: 'gray', marginLeft: 10 }}>{item.nutriText}</Text>
-            </View>
-          ))}
+const RecipeVideo = ({ route }) => {
+  const { TextHeading, Description } = route.params;
+  return (
+    <View style={{ flex: 1, backgroundColor: '#ebebeb' }}>
+      <Video
+        paused
+        source={{
+          uri: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        }}
+        style={{
+          width: Dimensions.get('window').width,
+          height: Dimensions.get('window').width * (9 / 16),
+          backgroundColor: 'black',
+        }}
+        controls
+        resizeMode="cover"
+      />
+      <ScrollView style={{ flex: 1, margin: 15 }}>
+        <View style={{ paddingVertical: 10 }}>
+          <Text style={{ fontSize: 25, fontWeight: 'bold' }}>{TextHeading}</Text>
+          <Text style={{ fontSize: 20 }}>{Description}</Text>
         </View>
-      </View>
-      <View style={{ height: 1, width: 370, backgroundColor: 'gray' }} />
-      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
-        Generally, nutrients are divided into two classes:
-      </Text>
-      <Text style={{ fontSize: 15 }}>{summary}</Text>
-    </ScrollView>
-  </View>
-);
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Nutritional Info.</Text>
+        <View
+          style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+          <Svg viewBox="0 0 200 200" height={200} width={200}>
+            <VictoryPie
+              labels={() => null}
+              colorScale={chartData.map(x => x.color)}
+              width={200}
+              height={200}
+              innerRadius={60}
+              data={chartData.map(x => ({ x: x.nutriText, y: x.weight }))}
+            />
+            <VictoryLabel
+              textAnchor="middle"
+              verticalAnchor="middle"
+              x={100}
+              y={100}
+              style={{ fontSize: 14 }}
+              text="71 Cal"
+            />
+          </Svg>
+          <View>
+            {chartData.map(item => (
+              <View style={{ flexDirection: 'row', paddingVertical: 5, alignItems: 'center' }}>
+                <View
+                  style={{
+                    height: 10,
+                    width: 30,
+                    backgroundColor: item.color,
+                    borderRadius: 5,
+                    marginRight: 10,
+                  }}
+                />
+                <Text>{`${item.weight}${item.unit}`}</Text>
+                <Text style={{ color: 'gray', marginLeft: 10 }}>{item.nutriText}</Text>
+              </View>
+            ))}
+          </View>
+        </View>
+        <View style={{ height: 1, width: 370, backgroundColor: 'gray' }} />
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+          Generally, nutrients are divided into two classes:
+        </Text>
+        <Text style={{ fontSize: 15 }}>{summary}</Text>
+      </ScrollView>
+    </View>
+  );
+};
 
-export default index;
+RecipeVideo.propTypes = {
+  route: PropTypes.shape({
+    params: PropTypes.shape({
+      TextHeading: PropTypes.string,
+      Description: PropTypes.string,
+    }),
+  }).isRequired,
+};
+
+export default RecipeVideo;
