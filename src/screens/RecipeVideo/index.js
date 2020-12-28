0af4ea1cb92f4e171bf589259c4ae0ecd2a236a9
patelyashdeep <patelyashdeep3110@gmail.com>
2020-12-28 13:02:07 +0530

    PieChart Added in Recipe Video

--- src/screens/RecipeVideo/index.js
+++ src/screens/RecipeVideo/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
-import { Dimensions, View } from 'react-native';
+import { Dimensions, Text, View } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import Video from 'react-native-video';
+import { VictoryPie } from 'victory-native';
 
+const summary = `Macronutrients: Macronutrients are required daily in large quantities. They include proteins, fats, carbohydrates, some minerals, and water.
+Micronutrients: Micronutrients are required daily in small quantities—in milligrams (one thousandth of a gram) to micrograms (one millionth of a gram). They include vitamins and certain minerals that enable the body to use macronutrients. These minerals are called trace minerals because the body needs only very small amounts.
+Foods consumed in the daily diet contain as many as 100,000 substances. But only 300 are classified as nutrients, and only 45 are classified as essential nutrients:
+
+1) Vitamins
+2) Minerals
+3) Some amino acids (components of protein)
+4) Some fatty acids (components of fats)`;
 const index = () => (
   <View style={{ flex: 1, backgroundColor: '#ebebeb' }}>
     <Video
@@ -16,6 +26,22 @@ const index = () => (
       controls
       resizeMode="cover"
     />
+    <ScrollView style={{ margin: 15 }}>
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Nutritional Info.</Text>
+      <VictoryPie
+        // theme={VictoryTheme.material}
+        colorScale={['tomato', 'orange', 'gold', 'cyan', 'navy']}
+        width={200}
+        height={200}
+        innerRadius={60}
+        data={[{ y: 100 }, { y: 100 }, { y: 100 }, { y: 100 }, { y: 100 }]}
+      />
+      <View style={{ height: 1, width: 370, backgroundColor: 'gray' }} />
+      <Text style={{ fontSize: 20, fontWeight: 'bold' }}>
+        Generally, nutrients are divided into two classes:
+      </Text>
+      <Text style={{ fontSize: 15 }}>{summary}</Text>
+    </ScrollView>
   </View>
 );
 
