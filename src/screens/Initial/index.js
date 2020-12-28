dcb464ab1fd85f8433bfd7cde03f8081aea0557f
shuklakhushi <shuklakhushi3110@gmail.com>
2020-12-28 11:55:10 +0530

    Login and Registration Button Done

--- src/screens/Initial/index.js
+++ src/screens/Initial/index.js
@@ -6,22 +6,15 @@ import { useTheme } from '@react-navigation/native';
 import PropTypes from 'prop-types';
 
 import { ImageBackground, Dimensions, KeyboardAvoidingView, View, Image } from 'react-native';
-import Form from '../../components/Form';
-import { initialValues, loginForm } from './fields';
 import LoginImage from '../../assets/pics/LoginImage.png';
 import TextEle from '../../components/TextEle';
 import RAButton from '../../components/RAButton';
 import FoodCourter from '../../assets/pics/FoodCourter.png';
-import GoogleLogo from '../../assets/icons/logo-google.svg';
 
 const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
 
-const Login = ({ navigation }) => {
+const Initial = ({ navigation }) => {
   const { colors } = useTheme();
-
-  const onSubmit = values => {
-    console.log(values);
-  };
 
   return (
     <ImageBackground source={LoginImage} style={{ height: windowHeight, width: windowWidth }}>
@@ -39,18 +32,18 @@ const Login = ({ navigation }) => {
           <TextEle variant="header2" style={{ color: 'white', alignItems: 'center' }}>
             Welcome to Food Courture
           </TextEle>
-          <Form initialValues={initialValues} fields={loginForm} onSubmit={onSubmit} />
         </View>
         <View style={{ width: '100%' }}>
           <RAButton
-            onPress={() => navigation.navigate('Home')}
-            style={{ opacity: 0.6, backgroundColor: colors.background }}>
-            <TextEle variant="buttonText">Continue</TextEle>
+            onPress={() => navigation.navigate('Login')}
+            style={{ opacity: 1, backgroundColor: colors.background }}>
+            <TextEle variant="buttonText">Login</TextEle>
           </RAButton>
           <View>
-            <RAButton style={{ opacity: 1, flexDirection: 'row' }}>
-              <GoogleLogo height={24} width={24} fill={colors.text} style={{ marginRight: 20 }} />
-              <TextEle variant="buttonText">Sign in with google</TextEle>
+            <RAButton
+              style={{ opacity: 1, flexDirection: 'row' }}
+              onPress={() => navigation.navigate('Registration')}>
+              <TextEle variant="buttonText">Register</TextEle>
             </RAButton>
           </View>
         </View>
@@ -59,10 +52,10 @@ const Login = ({ navigation }) => {
   );
 };
 
-Login.propTypes = {
+Initial.propTypes = {
   route: PropTypes.shape({}).isRequired,
   navigation: PropTypes.shape({
     navigate: PropTypes.func,
   }).isRequired,
 };
-export default Login;
+export default Initial;
