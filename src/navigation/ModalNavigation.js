2e570af39f468efb5fb4a4fb44db277f9b47211d
shuklakhushi <shuklakhushi3110@gmail.com>
2020-12-29 15:54:17 +0530

    Verification UI Done

--- src/navigation/ModalNavigation.js
+++ src/navigation/ModalNavigation.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Login from '../screens/Login';
 import Initial from '../screens/Initial';
 import Registration from '../screens/Registration';
+import Verification from '../screens/Verification';
 
 const ModalStack = createStackNavigator();
 
@@ -12,6 +13,7 @@ const ModalStackScreen = () => (
       headerShown: false,
     }}>
     <ModalStack.Screen name="Initial" component={Initial} />
+    <ModalStack.Screen name="Verification" component={Verification} />
     <ModalStack.Screen name="LoginModal" component={Login} />
     <ModalStack.Screen name="RegistrationModal" component={Registration} />
   </ModalStack.Navigator>
