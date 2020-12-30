9d31277721fe9c00c60203539aad7c15ae218061
harzh307 <harzh.patel@gmail.com>
2020-12-30 10:35:19 +0530

    RA-15 #done #comment compled Ui for purchase #time 2h

---
+++ src/components/PriceTag/data.js
@@ -0,0 +1,32 @@
+export default [
+  {
+    id: 1,
+    Sen: 'Duration',
+    Sub: 'Launching on 22nd December 6:00 PM',
+  },
+  {
+    id: 2,
+    Sen: 'Total Recipes covered',
+    Sub: '15 Varieties',
+  },
+  {
+    id: 3,
+    Sen: 'Video Validity',
+    Sub: '6 Days',
+  },
+  {
+    id: 4,
+    Sen: 'Written Recipe',
+    Sub: 'Forever',
+  },
+  {
+    id: 5,
+    Sen: 'Fees',
+    Sub: '₹1500',
+  },
+  {
+    id: 6,
+    Sen: 'Cooking Level',
+    Sub: 'Basic',
+  },
+];
--- src/screens/Initial/index.js
+++ src/screens/Initial/index.js
@@ -1,15 +1,11 @@
-/* eslint-disable no-console */
-/* eslint-disable react/no-unescaped-entities */
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import { useTheme } from '@react-navigation/native';
 import PropTypes from 'prop-types';
-
 import { ImageBackground, Dimensions, KeyboardAvoidingView, View, Image } from 'react-native';
-import LoginImage from '../../assets/pics/LoginImage.png';
+import LoginImage from '../../assets/images/LoginImage.png';
 import TextEle from '../../components/TextEle';
 import RAButton from '../../components/RAButton';
-import FoodCourter from '../../assets/pics/FoodCourter.png';
+import FoodCourter from '../../assets/images/FoodCourter.png';
 
 const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
 
