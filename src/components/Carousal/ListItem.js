35e3b8ec4c8172940aaffcaf685d73b341c1ebaa
imyagnesh <yagnesh.modh@gmail.com>
2021-02-08 15:47:30 +0530

    apply nullable

--- src/components/Carousal/ListItem.js
+++ src/components/Carousal/ListItem.js
@@ -17,7 +17,7 @@ const ListItem = ({ onRecipePress, item, cardWidth }) => {
       <ImageBackground
         style={{ height: 225, width: cardWidth - 10 }}
         imageStyle={{ borderRadius: 20 }}
-        source={{ uri: item.recipeImage.url }}>
+        source={{ uri: item?.recipeImage?.url }}>
         <View
           style={{
             flex: 1,
