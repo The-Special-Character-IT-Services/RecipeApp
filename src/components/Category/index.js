ddbe6d5833a973c2362f8dc60f6b6ea4d1ca96c9
patelyashdeep <patelyashdeep3110@gmail.com>
2021-02-08 21:37:54 +0530

    CategoryDetails Screen added

--- src/components/Category/index.js
+++ src/components/Category/index.js
@@ -1,14 +1,14 @@
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable global-require */
 import React from 'react';
-import { View } from 'react-native';
+import { Pressable, View } from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 import { useTheme } from '@react-navigation/native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import useCategoriesApi from '../../hooks/useCategoriesApiHook';
 import TextEle from '../TextEle';
 
-const Category = () => {
+const Category = ({ onCategoryDetails }) => {
   const { colors } = useTheme();
   const { data } = useCategoriesApi();
   if (!data?.categories) {
@@ -31,28 +31,30 @@ const Category = () => {
         contentContainerStyle={{ paddingHorizontal: 20 }}>
         {data?.categories.map(item => (
           <View key={item.id} style={{ marginHorizontal: 5, alignItems: 'center' }}>
-            <View
-              style={{
-                marginHorizontal: 5,
-                height: 50,
-                borderRadius: 12,
-                width: 50,
-                marginVertical: 10,
-                backgroundColor: item.id === 1 ? colors.primary : colors.card,
-                alignItems: 'center',
-                justifyContent: 'center',
-                shadowColor: colors.text,
-                shadowOffset: {
-                  width: 0,
-                  height: 4,
-                },
-                shadowOpacity: 0.3,
-                shadowRadius: 4.65,
-                elevation: 8,
-              }}>
-              <Icon name={item.iconName} size={24} color={colors.primary} />
-              {/* {item.icon(item.id === 1 ? colors.card : colors.primary)} */}
-            </View>
+            <Pressable onPress={onCategoryDetails}>
+              <View
+                style={{
+                  marginHorizontal: 5,
+                  height: 50,
+                  borderRadius: 12,
+                  width: 50,
+                  marginVertical: 10,
+                  backgroundColor: item.id === 1 ? colors.primary : colors.card,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  shadowColor: colors.text,
+                  shadowOffset: {
+                    width: 0,
+                    height: 4,
+                  },
+                  shadowOpacity: 0.3,
+                  shadowRadius: 4.65,
+                  elevation: 8,
+                }}>
+                <Icon name={item.iconName} size={24} color={colors.primary} />
+                {/* {item.icon(item.id === 1 ? colors.card : colors.primary)} */}
+              </View>
+            </Pressable>
             <View>
               <TextEle variant="caption">{item.name}</TextEle>
             </View>
---
+++ src/screens/CategoryDetails/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const CategoryDetails = () => (
+  <View>
+    <Text style={{ color: 'red' }}>Hello</Text>
+  </View>
+);
+
+export default CategoryDetails;
