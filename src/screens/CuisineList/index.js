d34a684dbcb2292eb454d3f6694d4a03c7899fbc
patelyashdeep <patelyashdeep3110@gmail.com>
2021-02-10 15:03:36 +0530

    SearchCategory UI

--- src/screens/CuisineList/index.js
+++ src/screens/CuisineList/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* eslint-disable react-native/no-inline-styles */
 import React, { useState } from 'react';
 import { ScrollView, View, Image, KeyboardAvoidingView } from 'react-native';
@@ -56,20 +55,5 @@ const Search = () => {
     </KeyboardAvoidingView>
   );
 };
-=======
-import TextEle from '@components/TextEle';
-import React from 'react';
-import { View } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
-
-const CuisineList = () => (
-  <View>
-    <TextEle>Filter List</TextEle>
-    <ScrollView contentContainerStyle={{ paddingHorizontal: 20 }}>
-      <View></View>
-    </ScrollView>
-  </View>
-);
->>>>>>> 5d42786d7b0da9714f9cc022123afa39ac94ffcb
 
 export default Search;
