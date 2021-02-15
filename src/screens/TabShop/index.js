1925ea027e9ed2ab8e701c034b6fcb932eb591a0
raj-patel-sys <patelrajj1904@gmail.com>
2021-02-15 16:52:16 +0530

    coming soon cart added

--- src/screens/TabShop/index.js
+++ src/screens/TabShop/index.js
@@ -1,15 +1,21 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { ScrollView } from 'react-native-gesture-handler';
-import ShopProduct from '../../components/ShopProduct';
-import Advertisement from '../../components/Advertisement';
+// import { ScrollView } from 'react-native-gesture-handler';
+// import ShopProduct from '../../components/ShopProduct';
+import LottieView from 'lottie-react-native';
+// import Advertisement from '../../components/Advertisement';
+import { View } from 'react-native';
+import TextEle from '@components/TextEle';
 
 const TabShop = () => (
-  <ScrollView style={{}}>
-    <Advertisement />
-    <ScrollView>
-      <ShopProduct />
-    </ScrollView>
-  </ScrollView>
+  <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <LottieView
+      source={require('@assets/lottie/45728-cooking-news-animation.json')}
+      style={{ height: 300, width: 300 }}
+      autoPlay
+      loop={false}
+    />
+    <TextEle variant="header1">Coming Soon</TextEle>
+  </View>
 );
 export default TabShop;
