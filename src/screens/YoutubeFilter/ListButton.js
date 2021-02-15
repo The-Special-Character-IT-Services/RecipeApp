b5d121d92f9f587e951caa84972de8f55e1da4a2
patelyashdeep <patelyashdeep3110@gmail.com>
2021-02-15 11:26:48 +0530

    Toggle done logic left

---
+++ src/screens/YoutubeFilter/ListButton.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+const ListButton = ({ label, isVisibleIcon, onPress }) => {
+  const [icon, setIcon] = useState('arrow-up-outline');
+  const onButtonPress = () => {
+    if (icon === 'arrow-up-outline') {
+      setIcon('arrow-down-outline');
+    } else {
+      setIcon('arrow-up-outline');
+    }
+    onPress();
+  };
+  return (
+    <RectButton onPress={onButtonPress}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+        <Text style={{ paddingVertical: 20, fontSize: 18, color: 'white' }}>{label}</Text>
+        {isVisibleIcon && (
+          <Icon
+            name={icon}
+            size={24}
+            color="red"
+            style={{ position: 'absolute', top: 23, right: 20 }}
+          />
+        )}
+      </View>
+    </RectButton>
+  );
+};
+
+export default ListButton;
--- src/screens/YoutubeFilter/index.js
+++ src/screens/YoutubeFilter/index.js
@@ -1,34 +1,29 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
-import Icon from 'react-native-vector-icons/Ionicons';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import ListButton from './ListButton';
 
-const YoutubeFilter = () => (
-  <View style={{ flex: 1, marginLeft: 20 }}>
-    <RectButton>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text style={{ paddingVertical: 20, fontSize: 18, color: 'white' }}>Publish Date</Text>
-        <Icon
-          name="arrow-down-outline"
-          size={24}
-          color="red"
-          style={{ position: 'absolute', top: 13, right: 10 }}
-        />
-      </View>
-    </RectButton>
-    <View style={{ height: 1, width: 370, backgroundColor: 'gray' }} />
-    <RectButton>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-        <Text style={{ paddingVertical: 20, fontSize: 18, color: 'white' }}>Likes</Text>
-      </View>
-    </RectButton>
-    <View style={{ height: 1, width: 370, backgroundColor: 'gray' }} />
-    <RectButton>
-      <View>
-        <Text style={{ paddingVertical: 20, fontSize: 18, color: 'white' }}>Views</Text>
-      </View>
-    </RectButton>
-    <View style={{ height: 1, width: 370, backgroundColor: 'gray' }} />
-  </View>
-);
+const YoutubeFilter = () => {
+  const [currentFilter, setCurrentFilter] = useState('Publish Date');
+  return (
+    <View style={{ flex: 1, marginLeft: 20 }}>
+      <ListButton
+        label="Publish Date"
+        isVisibleIcon={currentFilter === 'Publish Date'}
+        onPress={() => setCurrentFilter('Publish Date')}
+      />
+      <View style={{ height: 1, backgroundColor: 'gray' }} />
+      <ListButton
+        label="Likes"
+        isVisibleIcon={currentFilter === 'Likes'}
+        onPress={() => setCurrentFilter('Likes')}
+      />
+      <View style={{ height: 1, backgroundColor: 'gray' }} />
+      <ListButton
+        label="Views"
+        isVisibleIcon={currentFilter === 'Views'}
+        onPress={() => setCurrentFilter('Views')}
+      />
+    </View>
+  );
+};
 export default YoutubeFilter;
