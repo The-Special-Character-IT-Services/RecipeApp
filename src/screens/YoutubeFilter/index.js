80552d46f762248f52edf5df64e59fbfd5323797
patelyashdeep <patelyashdeep3110@gmail.com>
2021-02-15 10:40:13 +0530

    just icon added

--- src/screens/YoutubeFilter/index.js
+++ src/screens/YoutubeFilter/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const YoutubeFilter = () => (
   <View style={{ flex: 1, marginLeft: 20 }}>
     <RectButton>
       <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
         <Text style={{ paddingVertical: 20, fontSize: 18, color: 'white' }}>Publish Date</Text>
+        <Icon
+          name="arrow-down-outline"
+          size={24}
+          color="red"
+          style={{ position: 'absolute', top: 13, right: 10 }}
+        />
       </View>
     </RectButton>
     <View style={{ height: 1, width: 370, backgroundColor: 'gray' }} />
