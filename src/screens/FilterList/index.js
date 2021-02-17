4e5e4ef3b98109ee2a8fe97b95f62135e7ccf21b
shuklakhushi <shuklakhushi3110@gmail.com>
2021-02-17 16:08:49 +0530

    FilterList data looks and loadmore

--- src/screens/FilterList/index.js
+++ src/screens/FilterList/index.js
@@ -1,74 +1,104 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react-native/no-inline-styles */
-import React, { useState } from 'react';
-import { ScrollView, View, KeyboardAvoidingView } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { ScrollView, View, ActivityIndicator, FlatList } from 'react-native';
 import { useTheme } from '@react-navigation/native';
-import useSWR from 'swr';
-import { coursesCategoryQuery } from '@hooks/useCoursesApiHook';
+import useSWR, { useSWRInfinite } from 'swr';
+import { getInfiniteFilteredCourses } from '@hooks/useCoursesApiHook';
 import Image from 'react-native-fast-image';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import SearchBar from '../../components/Search';
 import TextEle from '../../components/TextEle';
 
+const ITEM_HEIGHT = 100;
+
 const FilterList = ({ route }) => {
   const { where } = route.params;
   const insets = useSafeAreaInsets();
-  const { data } = useSWR([coursesCategoryQuery(0, 5, 'updated_at:DESC', where)]);
+  const { data, error, isValidating, mutate, size, setSize } = useSWRInfinite(
+    getInfiniteFilteredCourses,
+  );
+
+  // const { data, isValidating } = useSWR([coursesCategoryQuery(0, 5, 'updated_at:DESC', where)]);
   const { colors } = useTheme();
   const [text, setText] = useState('');
   const onChangeText = val => {
     setText(val);
   };
+
+  const keyExtractor = useCallback(item => `${item?.id}`, []);
+
+  const renderItem = useCallback(
+    ({ item }) => (
+      <View
+        key={item.id}
+        style={{
+          flexDirection: 'row',
+          height: ITEM_HEIGHT,
+        }}>
+        <Image
+          source={{ uri: item?.image?.formats?.thumbnail?.url || item?.image?.url }}
+          style={{ height: ITEM_HEIGHT, width: ITEM_HEIGHT, borderRadius: 5 }}
+        />
+        <View
+          style={{
+            flex: 1,
+            paddingHorizontal: 10,
+          }}>
+          <TextEle variant="body1">{item.name}</TextEle>
+          <TextEle variant="body1">{item.caption}</TextEle>
+        </View>
+      </View>
+    ),
+    [],
+  );
+
+  const getItemLayout = useCallback(
+    (data, index) => ({
+      length: ITEM_HEIGHT,
+      offset: ITEM_HEIGHT * index,
+      index,
+    }),
+    [],
+  );
+
+  // const loadMore = useCallback(() => {
+  //   setSize(size + 1);
+  // }, [size, setSize]);
+
   return (
-    <KeyboardAvoidingView
-      style={{
-        paddingTop: insets.top,
-      }}>
+    <View>
       <View style={{ paddingTop: 10 }}>
         <SearchBar onChangeText={onChangeText} text={text} />
       </View>
-      <ScrollView
-        style={{
-          marginVertical: 10,
-          marginHorizontal: 10,
-          flexDirection: 'row',
-        }}>
-        {data?.courses.map(item => (
+      <FlatList
+        contentContainerStyle={{
+          margin: 15,
+        }}
+        data={data?.reduce((p, c) => [...p, ...c.courses], []) || []}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+        getItemLayout={getItemLayout}
+        removeClippedSubviews
+        ListFooterComponent={() => {
+          if (isValidating) {
+            return <ActivityIndicator animating size="large" color={colors.pr} />;
+          }
+          return null;
+        }}
+        ItemSeparatorComponent={() => (
           <View
-            key={item.id}
             style={{
-              flexDirection: 'row',
+              borderBottomWidth: 1,
+              borderBottomColor: colors.border,
               marginVertical: 10,
-              marginHorizontal: 10,
-            }}>
-            <Image
-              source={{ uri: item.image.url }}
-              style={{ height: 50, width: 50, borderRadius: 5 }}
-            />
-            <TextEle
-              variant="body1"
-              style={{
-                color: colors.text,
-                flexDirection: 'row',
-                marginHorizontal: 10,
-                marginVertical: 3,
-              }}>
-              {item.name}
-            </TextEle>
-            <TextEle
-              variant="body1"
-              style={{
-                color: colors.text,
-                flexDirection: 'row',
-                marginHorizontal: 10,
-                marginVertical: 3,
-              }}>
-              {item.caption}
-            </TextEle>
-          </View>
-        ))}
-      </ScrollView>
-    </KeyboardAvoidingView>
+            }}
+          />
+        )}
+        // onEndReached={loadMore}
+        // onEndReachedThreshold={0.5}
+      />
+    </View>
   );
 };
 
