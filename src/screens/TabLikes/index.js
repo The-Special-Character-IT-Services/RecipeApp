1b3e8754de2fe7001a40c6283026288b26506337
shuklakhushi <shuklakhushi3110@gmail.com>
2021-02-17 11:38:18 +0530

    Event and Like TabScreens complete

--- src/screens/TabLikes/index.js
+++ src/screens/TabLikes/index.js
@@ -1,70 +1,24 @@
-import React, { useMemo, useRef } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Dimensions, Image, TextInput, View } from 'react-native';
-import BottomSheet from '@gorhom/bottom-sheet';
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { useTheme } from '@react-navigation/native';
-import { useLayout } from '@react-native-community/hooks';
+import { View } from 'react-native';
+import SearchBar from '../../components/Search';
 import LikedRecipe from '../../components/LikedRecipe';
-import AllFood from '../../assets/images/AllFood.jpg';
-
-const { height: windowHeight, width: windowWidth } = Dimensions.get('window');
 
 const TabLikes = ({ navigation }) => {
-  const { colors } = useTheme();
-  const insets = useSafeAreaInsets();
-  const { onLayout, ...layout } = useLayout();
+  const [text, setText] = useState('');
 
-  const bottomSheetRef = useRef(null);
-  const snapPoints = useMemo(
-    () => [windowHeight * 0.6, windowHeight - layout.height - insets.top],
-    [layout.height, insets.top],
-  );
+  const onChangeText = val => {
+    setText(val);
+  };
 
   return (
     <View style={{ flex: 1 }}>
-      <View>
-        <Image
-          style={{
-            height: windowHeight * 0.5,
-            width: windowWidth,
-            position: 'absolute',
-            top: 0,
-            left: 0,
-          }}
-          source={AllFood}
-        />
-      </View>
-      <View onLayout={onLayout} style={{ paddingTop: insets.top, paddingBottom: 30 }}>
-        <TextInput
-          placeholder="Search your liked recipes"
-          placeholderTextColor={colors.text}
-          style={{
-            color: colors.text,
-            borderColor: colors.background,
-            borderWidth: 2,
-            borderRadius: 20,
-            paddingVertical: 10,
-            paddingHorizontal: 20,
-            margin: 20,
-          }}
-        />
-      </View>
-      <View style={{ height: 700 }}>
-        <BottomSheet
-          style={{ flex: 1 }}
-          onLayout={onLayout}
-          ref={bottomSheetRef}
-          snapPoints={snapPoints}
-          handleComponent={() => null}
-          topInset={insets.top}>
-          <LikedRecipe
-            onRecipeDetail={item => {
-              navigation.navigate('RecipeDetail', item);
-            }}
-          />
-        </BottomSheet>
-      </View>
+      <SearchBar onChangeText={onChangeText} value={text} clearText={() => setText('')} />
+      <LikedRecipe
+        onRecipeDetail={item => {
+          navigation.navigate('RecipeDetail', item);
+        }}
+      />
     </View>
   );
 };
